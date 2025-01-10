2617959da90d30421918930606f2b5ac2cac282b
Walid Msallem <walidmsallem@gmail.com>
2025-01-10 10:46:00 +0000

    create boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import ComplyCubeIntegration from './components/ComplyCubeIntegration'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ComplyCube Integration</h1>
+      <ComplyCubeIntegration />
+    </div>
+  )
+}
+
+export default App
