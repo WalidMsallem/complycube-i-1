944524e2e7d51f38ffb40819ab9c6cd70e3cef9d
Walid Msallem <walidmsallem@gmail.com>
2025-01-11 16:04:29 +0000

    add console to onComplete

--- src/integration-config/custom1.js
+++ src/integration-config/custom1.js
@@ -52,38 +52,6 @@ export const customConfig1 = (clientId) => ({
     },
   ],
   onComplete: async (data) => {
-    try {
-      const checks = [
-        ...([
-          'driving_license',
-          'tax_document',
-          'bank_statement',
-          'utility_bill',
-        ].includes(data.documentCapture.documentType)
-          ? [
-              {
-                documentId: data.documentCapture.documentId,
-                type: 'proof_of_address_check',
-              },
-            ]
-          : []),
-        {
-          type: 'document_check',
-          documentId: data.documentCapture.documentId,
-        },
-        { type: 'extensive_screening_check' },
-      ]
-
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_ENDPOINT}/api/checks`,
-        {
-          checks,
-          clientId,
-        }
-      )
-      return response.data
-    } catch (e) {
-      console.error(e || 'Something went wrong')
-    }
+    console.log('onComplete capture ', data)
   },
 })
